75af47c74c25944110e35cf6e97e837553ba9544
Eunice <euchiri@gmail.com>
2017-10-13 15:19:23 +0000

    test cases for division

---
+++ division.py
@@ -0,0 +1,6 @@
+def divide(a,b):
+  
+  return a / b
+    
+ ''' except ZeroDivisionError:
+        print("Cannot divide by zero")'''
--- testdivision.py
+++ testdivision.py
@@ -4,8 +4,17 @@ from division import divide
 class test_division(unittest.TestCase):
     
     def test_division(self):
-        self.assertEquals((4,2),2)
+        self.assertEquals(divide(4,2),2)
         
-    def test_division_negative(self):
-        self.assert
+    def test_negative_division(self):
+        self.assertEquals(divide(-9,-3),3)
         
+    def test_float_division(self):
+        self.assertEquals(divide(6.25,2.5),2.5)
+    
+
+    
+        
+        
+if __name__ == '__main__':
+    unittest.main()
