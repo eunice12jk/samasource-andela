607bde7598b7965d8d85512cb29e480193c2edae
Eunice <euchiri@gmail.com>
2017-10-13 14:15:20 +0000

    Added test cases

---
+++ testdivision.py
@@ -0,0 +1,11 @@
+import unittest
+from division import divide
+
+class test_division(unittest.TestCase):
+    
+    def test_division(self):
+        self.assertEquals((4,2),2)
+        
+    def test_division_negative(self):
+        self.assert
+        
