e9fff5e3dfd091c7da474aff593c325536047dca
Eunice <euchiri@gmail.com>
2017-10-13 09:08:10 +0000

    Sum TestCase

---
+++ testSum.py
@@ -0,0 +1,10 @@
+import unittest
+from Sum import sum
+
+class test_sum(unittest.TestCase):
+  
+  def test_sum(self):
+    self.assertEquals(sum(2,3),5)
+    
+if __name__ == "__main__":
+    unittest.main()
